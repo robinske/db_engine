26c9ff3c90b3c852fb64d6c9141d729311876b7c
Kelley <krobinson08@gmail.com>
2013-04-14 22:35:34 -0700

    short term memory loss

--- client.go
+++ client.go
@@ -26,32 +26,53 @@ func reader(r io.Reader) {
   }
 }
 
-func talkToDB() (cacheData) {
-  data := make(cacheData)
-  fmt.Println("YOU'VE TALKED TO THE DB")
-  return data
-}
-
-func get(key, instruct string) {
-  fmt.Printf("You called the %s function\n", instruct)
-  fmt.Printf("We'll get the value of key %s\n", key)
+func talkToDictionary(instruct, key, value string, dictionary cacheData) (cacheData) {
   
-  //fmt.Println(data[key])
+  
+  // ints := []int{1,2,3}
+  // values := []string{instruct, key, value}
+
+  // dict2 := make(map[ints int] values string)
+  // fmt.Println(dict2)
+
+  switch instruct {
+    case "GET": get(key, instruct, dictionary)
+    case "PUT": put(key, value, dictionary)
+    //case "SAVE": save(key, value, instruct, dictionary)
+    default: fmt.Println("try again idiot")
+  }
+
+  fmt.Println("YOU'VE TALKED TO THE DB")
+  fmt.Println(dictionary)
+  return dictionary
 }
 
-func put(key, value string) {
+func get(key, instruct string, dictionary cacheData) {
+  getKey := key
+  getInstruct := instruct
+
+  fmt.Printf("You called the %s function\n", getInstruct)
+  fmt.Printf("We'll get the value of key %s\n", getKey)
+  fmt.Printf(dictionary[getKey]) ////// WHYYYYYY
+  fmt.Println("should have returned the value")
+}
+
+func put(key, value string, dictionary cacheData) {
   // fmt.Printf("You called the %s function\n", instruct)
-  fmt.Printf("We'll put %s:%s in the dictionary\n", key, value)
-  //data := cacheData{key:value}
+  putKey := key
+  putValue := value
+
+  fmt.Printf("We'll put %s:%s in the dictionary\n", putKey, putValue)
+  dictionary[putKey] = putValue
   //fmt.Println(data[key]) // should print value
 }
 
-func save(key, value, instruct string) {
-  fmt.Printf("You called the %s function\n", instruct)
-  fmt.Printf("We'll save %s:%s from dictionary to disk\n", key, value)
-}
+//func save(key, value, instruct string) {
+//   fmt.Printf("You called the %s function\n", instruct)
+//   fmt.Printf("We'll save %s:%s from dictionary to disk\n", key, value)
+// }
 
-func parseRequest(message string) (string) {
+func parseRequest(message string) (string, cacheData) {
   msgSplit := strings.Split(message, " ")
 
   //fmt.Printf("%s", message)
@@ -60,27 +81,21 @@ func parseRequest(message string) (string) {
   instruct := msgSplit[0]
   key := msgSplit[1]
   value := msgSplit[2]
+  // value := msgSplit[2:]
+  // if len(value) == 0 {
+  //   return
+  // }
 
   // if len(msgSplit) < 2 {
     // handle error
   // }
   // database := "database" // could be a "FROM" statement later
+  var dictionary = make(cacheData) //{key:value}
+  fmt.Println("printing base dictionary", dictionary)
 
-  
+  talkToDictionary(instruct, key, value, dictionary)
 
-  switch instruct {
-    case "GET": get(key, instruct)
-    case "PUT": put(key, value)
-    case "SAVE": save(key, value, instruct)
-    default: fmt.Println("try again idiot")
-  }
-
-  return value
-}
-
-func addRequestToDict(message string) {
-
-
+  return value, dictionary
 }
 
 func main() {
@@ -102,7 +117,9 @@ func main() {
     }
     if message != "" {
         parseRequest(message)
+        // DO THE SHIT HERE
         _,err := c.Write([]byte(message))
+
         if err != nil {
           log.Fatal(err)
           break
