faccc0bb37f15ac79809c6ba58711fd1408f3d2d
Kelley <krobinson08@gmail.com>
2013-04-24 16:07:23 -0700

    cleaning up

--- flatten/unflatten.go
+++ flatten/unflatten.go
@@ -1,148 +1,78 @@
-package main
-
-import (
-	"fmt"
-	"encoding/json"
-	"log"
-	"io/ioutil"
-	"strings"
-	"strconv"
-)
-
+var cacheData = Dictionary {}
 type Dictionary map[string]interface{}
 
-var cacheData = Dictionary {}
-var lkey = ""
-var flattened = make(map[string]interface{})
-var jsonString = `{`
-
-func load(filename string) {
-    fileContents, err := ioutil.ReadFile(filename)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    mappedJSON := decodeJSON(fileContents)
-
-    flatten(mappedJSON, lkey, &flattened)
-
-    for key, value := range flattened {
-        fmt.Printf("%v:%v\n", key, value)
-    }
-    
-    for k,v := range flattened {
-        k = strings.ToUpper(k)
-        if _, ok := v.(string); ok {
-            v = strings.ToUpper(v.(string))
-            cacheData[k] = v.(string)
-        } else if _, ok := v.(float64); ok {
-            v = v.(float64)
-            cacheData[k] = v.(float64)
-        } else if _, ok := v.(bool); ok {
-            v = v.(bool)
-            cacheData[k] = v.(bool)
-        } else {
-            fmt.Println("JSON file format error")
-        }
-    }
-}
-
-func decodeJSON(encodedJSON []byte) map[string]interface{} {
-    decoded := map[string]interface{} {}
-    err := json.Unmarshal(encodedJSON, &decoded)
-    if err != nil {
-        log.Fatal(err)
-    }
-    return decoded
-}
-
-func flatten(inputJSON map[string]interface{}, lkey string, flattened *map[string]interface{}) {
-    for rkey, value := range inputJSON {
-        key := lkey+rkey
-        if _, ok := value.(string); ok {
-            (*flattened)[key] = value.(string)
-        } else if _, ok := value.(float64); ok {
-            (*flattened)[key] = value.(float64)
-        } else if _, ok := value.(bool); ok {
-            (*flattened)[key] = value.(bool)
-        } else if _, ok := value.([]float64); ok { // type check for a list of integers not working - is this valid JSON though?
-            (*flattened)[key] = value.([]float64)
-        } else if _, ok := value.([]interface{}); ok {
-            for i := 0; i<len(value.([]interface{})); i++ {
-                if _, ok := value.([]string); ok {
-                    stringIndex := string(i)
-                    (*flattened)[stringIndex] = value.(string)
-                } else {
-                    flatten(value.([]interface{})[i].(map[string]interface{}), 
-                            key+":"+strconv.Itoa(i)+":", flattened)
-                }
-            }
-        } else {
-            flatten(value.(map[string]interface{}), key+":", flattened)
-        }
-    }
-}
-
-var uniqueKeys = map[string]string {}
-
-
-func unflatten(flattened map[string]interface{}, jsonString string) string {
-	
-	for k,v := range flattened {
-		if strings.Contains(k, ":") == false {
-			if _, ok := v.(string); ok {
-				jsonString = jsonString+"\""+k+"\":\""+v.(string)+"\","
-			} else if _, ok := v.(float64); ok {
-				jsonString = jsonString+"\""+k+"\":"+strconv.FormatFloat(v.(float64), 'f', -1, 64)+","
-			} else if _, ok := v.(bool); ok {
-				jsonString = jsonString+"\""+k+"\":"+strconv.FormatBool(v.(bool))+","
-			} else {
-				fmt.Println("da fuck happened?")
-			}
+func unflatten(jsonString string) {
+	for k, v := range cacheData {
+		if strings.Contains(k, ":") {
+			fmt.Println("split that shit")
 		} else {
-			splitKey := strings.SplitN(k, ":", 2)
-			newKey := splitKey[0]
-			newValue := strings.Join(splitKey[1:], " ")
-
-			// listIndex, _ := strconv.Atoi(newKey)
-			listElems := []string{}
-			listElems = append(listElems, newValue)
-			
-			fmt.Println(listElems)
-			uniqueKeys[newKey] = ""
-
-			innerCache := map[string]interface{}{}
-			innerCache[newValue] = v
-			unflatten(innerCache, jsonString)
-			fmt.Println("newKey: ", newKey, "newValue: ", newValue)
+			fmt.Println("insert this into the string")
 		}
 	}
-	for key := range uniqueKeys {
-		jsonString = jsonString+"\""+key+"\":NESTED VALUES HERE,"
-	}
-	jsonString = jsonString+"}"
-	return jsonString
-
 }
 
-func encode(sjson string) {
-	b, err := json.Marshal(sjson)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(b)
-	fmt.Println(string(b))
-}
 
-func main() {
-	
-	load("example.json")
-	sjson := unflatten(cacheData, jsonString)
+"""
+if key does not contain ':'
+	key:value in the top level of json
+else:	
+	split it
+	if split value can be converted to a number, then it's a list index
+"""
 
-	fmt.Println(sjson)
-	//fmt.Println(jsonString)
-	// sjson := `{"Name":"Alice", "Body":"Hello", "Time":{"Day":"Monday", "Hour":"Afternoon"}}`
 
-	//encode(sjson)
-	
+
+key:value in the top level of json
+
+"""
+
+batters:batter:1:type:Chocolate
+id:0001
+topping:2:id:5005
+topping:0:id:5001
+batters:batter:0:type:Regular
+batters:batter:0:id:1001
+batters:batter:2:type:Blueberry
+type:donut
+topping:6:id:5004
+topping:5:id:5003
+ppu:0.55
+batters:batter:1:id:1002
+
+topping:1:type:Glazed
+batters:batter:3:type:Devil's Food
+batters:batter:2:id:1003
+name:Cake
+topping:3:type:Powdered Sugar
+topping:1:id:5002
+topping:5:type:Chocolate
+topping:6:type:Maple
+topping:4:type:Chocolate with Sprinkles
+topping:4:id:5006
+topping:0:type:None
+topping:2:type:Sugar
+batters:batter:3:id:1004
+topping:3:id:5007
+
+"""
+
+
+
+{
+	"id": "0001",
+	"type": "donut",
+	"name": "Cake",
+	"ppu": 0.55,
+	"batters":{"batter":[{ "id": "1001", "type": "Regular" },{ "id": "1002", "type": "Chocolate" },{ "id": "1003", "type": "Blueberry" },{ "id": "1004", "type": "Devil's Food" }]
+		},
+	"topping":
+		[
+			{ "id": "5001", "type": "None" },
+			{ "id": "5002", "type": "Glazed" },
+			{ "id": "5005", "type": "Sugar" },
+			{ "id": "5007", "type": "Powdered Sugar" },
+			{ "id": "5006", "type": "Chocolate with Sprinkles" },
+			{ "id": "5003", "type": "Chocolate" },
+			{ "id": "5004", "type": "Maple" }
+		]
 }
