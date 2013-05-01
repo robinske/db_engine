ba02cb0b155222c916f04993986557ed97e10c6e
Kelley <krobinson08@gmail.com>
2013-04-30 21:12:37 -0700

    starting web app

--- json2csv.py
+++
@@ -1,7 +0,0 @@
-import csv
-import json
-
-f = open('sample.csv', 'r')
-reader = csv.DictReader(f, fieldnames = ( "id","name","lat","lng" ) )
-out = json.dumps( [ row for row in reader ] )
-print out
